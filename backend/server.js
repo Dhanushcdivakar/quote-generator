96503b9d5550087f9ae64828b0192b3227557d82
Dhanush C D <152018990+Dhanushcdivakar@users.noreply.github.com>
2025-08-12 21:17:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World! Your backend is successfully deployed.');
+});
 // Routes
 app.use('/api', quoteRoutes);
 
